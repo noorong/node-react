528048ad8f21b0fd14e54bfd74fd70efeb331a92
JaeHun Park <wogns205@gmail.com>
2024-02-11 21:11:40 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+const { User } = require("./models/User")
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://oceanwing:abcd1234@cluster0.3skgbwo.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log("MongoDB Connected!!!"))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+
+app.post('/register', async (req, res) => {
+
+  const user = new User(req.body)
+  try {
+    await user.save();
+    return res.status(200).json({success: true})
+  } catch (err) {
+      return res.json({ success: false, err})
+  }
 })
 
 app.listen(port, () => {
